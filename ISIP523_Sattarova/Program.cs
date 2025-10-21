0be294570d9a9ed28c26383b3557edfc9efdaea8
234801 <234801@KB37-303-C6>
2025-10-21 09:45:44 +0300

    Декомпозиция: Написала класс оружия и брони и создал экземпляры класса

--- ISIP523_Sattarova/Program.cs
+++ ISIP523_Sattarova/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Orug
+{
+    public string Name { get; set; }
+    public int uron { get; set; }
+
+    public Orug(string name, int damage)
+    {
+        Name = name;
+        uron = damage;
+    }
+}
+
+public class Brone
+{
+    public string Name { get; set; }
+    public double uroven { get; set; }
+
+    public Brone(string name, double defense)
+    {
+        Name = name;
+        uroven = defense;
+    }
+}
