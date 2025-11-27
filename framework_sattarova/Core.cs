fa366169caf87c3d11eb2f5246bd12c5ba1de7c1
234801 <234801@KB37-303-C1>
2025-11-27 15:03:57 +0300

    подключила бд

---
+++ framework_sattarova/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace framework_sattarova
+{
+    internal class Core
+    {
+        public static pr8_sattarovaEntities Context = new pr8_sattarovaEntities();
+
+    }
+}
